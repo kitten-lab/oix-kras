312d67a0ef9d7fa2913f807a9cb4a79226fc9484
Dani 'Kat' Leve <the.danibobby@gmail.com>
2026-04-11 17:21:16 -0400

    Rosewood8  >| Leaf: -- >| COMMIT 2 >| D17:T21:N16 on EPO7 GAIA R2026 ED04:ET6:EN11
    Affected files:
    platform/k/configs/xX_clrRouters_Xx.php
    platform/k/tools/plogBasic/actorAdd 1.php.php

--- platform/k/configs/xX_clrRouters_Xx.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$GLOBALS['kinit'] = 'k/incl/inits/';
-$GLOBALS['ktool'] = 'k/tools/';
-$GLOBALS['k']['init'] = 'k/incl/inits/';
-$GLOBALS['k']['config'] = 'k/configs/';
-$GLOBALS['k']['systems'] = 'k/systems/';
-?>
